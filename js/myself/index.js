4674b3ed39394611936424dfa2de52f7f2f50ffe
romaticbro <96171474+romaticbro@users.noreply.github.com>
2023-10-07 20:24:31 +0800

    Add files via upload

---
+++ js/myself/index.js
@@ -0,0 +1,60 @@
+$(function(){
+
+// 当前时间及日期设置
+  getNowTime();
+  getCountDown();
+  setInterval(getNowTime,1000);  //设置时间间隔一秒变化一次 不设置需要刷新才改变
+  setInterval(getCountDown,1000);
+
+  // 获取当前时间函数
+  function getNowTime(){
+    var date = new Date();
+
+    var s = date.getSeconds();
+    var minutes = date.getMinutes();
+    var h = date.getHours();
+    var d = date.getDate();
+    var month = date.getMonth() + 1;
+    var y = date.getFullYear();
+    s = s >= 10 ? s : '0' + s;
+    minutes = minutes >= 10 ? minutes : '0' + minutes;
+    h = h >= 10 ? h : '0' + h;
+    d = d >= 10 ? d : '0' + d;
+    month = month >= 10 ? month : '0' + month;
+    y = y >= 10 ? y : '0' + y;
+
+    var arr=[y,month,d,h,minutes,s];
+	//使用eq方法选取第一个list通过index填充list下面第一组span
+    $(".count-down .list").eq(0).children("span").each(function(index,domEle){
+      $(domEle).text(arr[index]);
+    })
+  }
+
+  // 获取倒计时函数
+  function getCountDown(){
+    var inputTime = +new Date('2025-7-1 0:0:0'); // 输入时间戳 1970年到2025-7-1的总毫秒
+    var nowTime = +new Date(); // 当前时间戳 1970年现在的总毫秒
+    var remainTime = (inputTime - nowTime) / 1000; // 剩余时间总的秒数
+    var s = parseInt(remainTime % 60);
+    var minutes = parseInt(remainTime / 60 % 60);
+    var h = parseInt(remainTime / 60 / 60 % 24);
+    var d = parseInt(remainTime / 60 / 60 / 24 % 30);
+    var month = parseInt(remainTime / 60 / 60 / 24 / 30 % 12);
+    var y = parseInt(remainTime / 60 / 60 / 24 / 30 / 12);
+
+	//如果输出的值小于10就在值前面加0 eg:输出就 就加0=09;
+    s = s >= 10 ? s : '0' + s;
+    minutes = minutes >= 10 ? minutes : '0' + minutes;
+    h = h >= 10 ? h : '0' + h;
+    d = d >= 10 ? d : '0' + d;
+    month = month >= 10 ? month : '0' + month;
+    y = y >= 10 ? y : '0' + y;
+
+    var arr=[y,month,d,h,minutes,s];
+	//使用eq方法选取第二个list通过index填充list下面第二组span
+    $(".count-down .list").eq(1).children("span").each(function(index,domEle){
+      $(domEle).text(arr[index]);
+    })
+  }
+  
+})
---
+++ js/school/index.js
@@ -0,0 +1,101 @@
+$(function(){
+
+  // banner 的 详细 通过this.index获取当前盒子的下标 通过下标给当前盒子添加class选择器的效果 同级兄弟删除class 中间部分
+  $(".detail .list .one").on({
+    "click":function(){
+      var index=$(this).index();
+      $(this).addClass("current").siblings().removeClass("current");
+      $(".detail .list .one i img").stop().slideUp(500).eq(index).stop().slideDown(750);
+    }
+  },".items-img");
+  $(".detail .list .two .items-img").on({
+    "click":function(){
+      var index=$(this).index();
+      $(this).addClass("current").siblings().removeClass("current");
+      $(".detail .list .two i img").stop().slideUp(500).eq(index).stop().slideDown(750);
+    }
+  });
+  $(".detail .list .three .items-img").on({
+    "click":function(){
+      var index=$(this).index();
+      $(this).addClass("current").siblings().removeClass("current");
+      $(".detail .list .three i img").stop().slideUp(500).eq(index).stop().slideDown(750);
+    }
+  });
+  $(".detail .list .four .items-img").on({
+    "click":function(){
+      var index=$(this).index();
+      $(this).addClass("current").siblings().removeClass("current");
+      $(".detail .list .four i img").stop().slideUp(500).eq(index).stop().slideDown(750);
+    }
+  });
+  $(".detail .list .five .items-img").on({
+    "click":function(){
+      var index=$(this).index();
+      $(this).addClass("current").siblings().removeClass("current");
+      $(".detail .list .five i img").stop().slideUp(500).eq(index).stop().slideDown(750);
+    }
+  });
+  $(".detail .list .six .items-img").on({
+    "click":function(){
+      var index=$(this).index();
+      $(this).addClass("current").siblings().removeClass("current");
+      $(".detail .list .six i img").stop().slideUp(500).eq(index).stop().slideDown(750);
+    }
+  });
+  // 照片墙效果
+  
+  // 全部照片设置
+  var x = $(".photo-wall").width();
+  var y = $(".photo-wall").height();
+  //页面一打开就移动图片位置
+  moveImg($(".photo-wall .items"));
+  // 当窗口大小调整时 更新 x y的值 同时重置 照片位置
+  $(window).resize(function(){
+    x = $(".photo-wall").width();
+    y = $(".photo-wall").height();
+	console.log(x,y)
+    moveImg($(".photo-wall .items"));
+  });
+  // 当前照片 点击事件，以及 其兄弟设置
+  $(".photo-wall").on({
+    "click":function(){
+      var imgX=x/2-$(this).width()/2;
+      var imgY=y/2-$(this).height()/2;
+      $(this).css("transform","rotate(0deg)").css("transform","scale(1.4)").css("z-index","1");
+      $(this).stop().animate({
+        left:imgX,
+        top:imgY,
+      },350,"linear");
+	  //兄弟标签index变为0 保证当前标签是第一层不会被其他图片覆盖
+      $(this).siblings(".items").css("z-index",0);
+	  //再次调用函数 使当前图片为第一层是其他图片也在移动 
+      moveImg($(this).siblings(".items"))  
+    }
+  },".items")
+  
+  // 随机数生成函数
+  function randomNum(minNum,maxNum){ 
+    switch(arguments.length){ 
+        case 1: 
+            return parseInt(Math.random()*minNum+1,10); 
+        break; 
+        case 2: 
+            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+        break; 
+            default: 
+                return 0; 
+            break; 
+    } 
+  }
+  
+  // 随机移动图片函数
+  function moveImg(Ele){
+    for(var i=0;i<Ele.length;i++){
+      Ele.eq(i).stop().animate({
+        left:randomNum(0,x*.8),
+        top:randomNum(0,y*.8),
+      },350,"linear").css("transform","rotate("+randomNum(0,90)+"deg)").css("transitionDuration",".35s").css("transitionTimingFunction","linear");
+    }
+  }
+})
